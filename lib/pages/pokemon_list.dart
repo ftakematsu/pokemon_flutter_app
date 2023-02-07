37ff8bb0af2a76a12adf274521bc72dd0f968056
Fabio Matsunaga <50036053+ftakematsu@users.noreply.github.com>
2023-02-06 22:24:26 -0300

    Lista com filtro

--- lib/pages/pokemon_list.dart
+++ lib/pages/pokemon_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:pokemon_flutter_app/consts/api.dart';
 import 'package:pokemon_flutter_app/pages/pokemon_info.dart';
 
@@ -23,8 +22,50 @@ class PokemonList extends StatefulWidget {
 
 class PokemonListState extends State<PokemonList> {
   
-  late List<Pokemon> list;
+  List<Pokemon> list = [];
   final myController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    list = listaFiltrada();     
+  }
+
+  List<Pokemon> listaFiltrada() {
+    List<Pokemon> outputList = list.where((pokemon) => pokemon.name.toLowerCase().contains(myController.text.toLowerCase())).toList();
+    return outputList;
+  }
+
+  Expanded areaListaPokemons() {
+    list = listaFiltrada();
+    if (list.isEmpty) {
+      return Expanded(
+        child: Text("Não foi encontrado nenhum Pokemom com o nome ${myController.text}"),
+      );
+    }
+    else {
+        return Expanded(
+                child: ListView.builder(
+                  itemCount: list.length, 
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      tileColor: Colors.blueAccent,
+                      leading: Image.network("${Api.pokemonPic}${list[index].num}.png"),
+                      title: Text("${list[index].name} "),
+                      subtitle: Text(list[index].getTypeString()),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => PokemonInfo(pokemon: list[index])),
+                          );
+                      },
+                    );
+                  }
+                ),
+              );
+    }
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,52 +79,18 @@ class PokemonListState extends State<PokemonList> {
                 TextFormField(
                   decoration: const InputDecoration(
                     border: UnderlineInputBorder(), // OutlineInputBorder(),
-                    labelText: 'Qual seu nome?',
+                    labelText: 'Nome do Pokemon',
                   ),
                   controller: myController,
+                  onChanged: (text) {
+                    setState(() {
+                      print('Você digitou: ${myController.text}');
+                    });
+                  },
                 ),
-                Expanded(
-                  child: ListView.builder(
-                  itemCount: list.length, 
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: Image.network("${Api.pokemonPic}${list[index].num}.png"),
-                      title: Text("${list[index].name} "),
-                      subtitle: Text(list[index].getTypeString()),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => PokemonInfo(pokemon: list[index])),
-                          );
-                      },
-                    );
-                  }
-                ),
-                )
+                areaListaPokemons()
             ],
           ),
       );
   }
-
-
 }
-
-/*
-
-ListView.builder(
-          itemCount: list.length, 
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: Image.network("${Api.pokemonPic}${list[index].num}.png"),
-              title: Text("${list[index].name} "),
-              subtitle: Text(list[index].getTypeString()),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => PokemonInfo(pokemon: list[index])),
-                  );
-              },
-            );
-          }
-        )
- */
